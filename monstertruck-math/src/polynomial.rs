30722e4ec79b3205b7aec1c5b2f1cafcbee1c5f9
Builder Chevalier <builder@embedded-knight.com>
2026-03-16 12:21:46 +0100

    feat(polynomial): implement all five polynomial solvers ported from matext4cgmath

--- monstertruck-math/src/polynomial.rs
+++ monstertruck-math/src/polynomial.rs
@@ -7,28 +7,159 @@ use crate::traits::BaseFloat;
 use num_complex::Complex;
 
 /// Solve x^2 + ax + b = 0.
-pub fn solve_quadratic<F: BaseFloat>(_a: F, _b: F) -> [Complex<F>; 2] {
-    todo!()
+pub fn solve_quadratic<F: BaseFloat>(a: F, b: F) -> [Complex<F>; 2] {
+    let two = F::one() + F::one();
+    let four = two + two;
+    let det = a * a - four * b;
+    match det >= F::zero() {
+        true => {
+            let h = num_traits::Float::sqrt(det);
+            [
+                Complex::new((-a - h) / two, F::zero()),
+                Complex::new((-a + h) / two, F::zero()),
+            ]
+        }
+        false => {
+            let h = num_traits::Float::sqrt(-det);
+            [Complex::new(-a, h) / two, Complex::new(-a, -h) / two]
+        }
+    }
 }
 
 /// Solve x^3 + px + q = 0 (depressed cubic).
-pub fn pre_solve_cubic<F: BaseFloat>(_p: F, _q: F) -> [Complex<F>; 3] {
-    todo!()
+pub fn pre_solve_cubic<F: BaseFloat>(p: F, q: F) -> [Complex<F>; 3] {
+    let two = F::one() + F::one();
+    let three = two + F::one();
+    let sqrt3_2 = num_traits::Float::sqrt(three) / two;
+    let omega = Complex::new(-F::one() / two, sqrt3_2);
+    let omega2 = Complex::new(-F::one() / two, -sqrt3_2);
+    let eps_2 = num_traits::Float::sqrt(num_traits::Float::epsilon());
+
+    let p_3 = p / three;
+    let q_2 = q / two;
+    let alpha2 = q_2 * q_2 + p_3 * p_3 * p_3;
+    let (x, y) = match alpha2 >= F::zero() {
+        true => {
+            let alpha = num_traits::Float::sqrt(alpha2);
+            let tmpx = -q_2 - alpha;
+            let tmpy = -q_2 + alpha;
+            (
+                Complex::new(
+                    num_traits::Float::signum(tmpx) * num_traits::Float::powf(num_traits::Float::abs(tmpx), F::one() / three),
+                    F::zero(),
+                ),
+                Complex::new(
+                    num_traits::Float::signum(tmpy) * num_traits::Float::powf(num_traits::Float::abs(tmpy), F::one() / three),
+                    F::zero(),
+                ),
+            )
+        }
+        false => {
+            let alphai = num_traits::Float::sqrt(-alpha2);
+            (
+                Complex::powf(Complex::new(-q_2, alphai), F::one() / three),
+                Complex::powf(Complex::new(-q_2, -alphai), F::one() / three),
+            )
+        }
+    };
+    let mut res = [x + y, omega * x + omega2 * y, omega2 * x + omega * y];
+    // Precision by Newton method.
+    res.iter_mut().for_each(|x| {
+        let mut f = *x * *x * *x + *x * p + q;
+        let mut f_prime = *x * *x * three + p;
+        while f.norm() > eps_2 * f_prime.norm() {
+            if f_prime.norm() < eps_2 {
+                return;
+            }
+            *x -= f / f_prime;
+            f = *x * *x * *x + *x * p + q;
+            f_prime = *x * *x * three + p;
+        }
+    });
+    res
 }
 
 /// Solve x^3 + ax^2 + bx + c = 0.
-pub fn solve_cubic<F: BaseFloat>(_a: F, _b: F, _c: F) -> [Complex<F>; 3] {
-    todo!()
+pub fn solve_cubic<F: BaseFloat>(a: F, b: F, c: F) -> [Complex<F>; 3] {
+    let two = F::one() + F::one();
+    let three = two + F::one();
+    let twenty_seven = three * three * three;
+    let p = b - a * a / three;
+    let q = c - a * b / three + two * a * a * a / twenty_seven;
+    let mut res = pre_solve_cubic(p, q);
+    res.iter_mut().for_each(|x| {
+        *x -= a / three;
+    });
+    res
 }
 
 /// Solve x^4 + px^2 + qx + r = 0 (depressed quartic).
-pub fn pre_solve_quartic<F: BaseFloat>(_p: F, _q: F, _r: F) -> [Complex<F>; 4] {
-    todo!()
+pub fn pre_solve_quartic<F: BaseFloat>(p: F, q: F, r: F) -> [Complex<F>; 4] {
+    let one = F::one();
+    let two = one + one;
+    let four = two + two;
+    let eps_2 = num_traits::Float::sqrt(num_traits::Float::epsilon());
+
+    let a = two * p;
+    let b = p * p - four * r;
+    let c = -q * q;
+    let f = solve_cubic(a, b, c);
+    let a = f[0].sqrt() / two;
+    let b = f[1].sqrt() / two;
+    let c = f[2].sqrt() / two;
+
+    // SAFETY: The iterator always yields exactly 8 elements, so min_by always
+    // returns `Some`.
+    let mut res = (0..8i32)
+        .map(|i| {
+            let a = a * num_traits::Float::powi(-F::one(), i % 2);
+            let b = b * num_traits::Float::powi(-F::one(), (i / 2) % 2);
+            let c = c * num_traits::Float::powi(-F::one(), (i / 4) % 2);
+            [-a - b - c, -a + b + c, a - b + c, a + b - c]
+        })
+        .map(|x| {
+            let f = x
+                .iter()
+                .map(|t| (t * t * t * t + t * t * p + t * q + r).norm_sqr())
+                .max_by(|x, y| x.partial_cmp(y).unwrap())
+                .unwrap();
+            (x, f)
+        })
+        .min_by(|x, y| x.1.partial_cmp(&y.1).unwrap())
+        .unwrap()
+        .0;
+    // Refinement by Newton method.
+    res.iter_mut().for_each(|x| {
+        let mut f = *x * *x * *x * *x + *x * *x * p + *x * q + r;
+        let mut f_prime = *x * *x * *x * four + *x * p * two + q;
+        while f.norm() > eps_2 * f_prime.norm() {
+            if f_prime.norm() < eps_2 {
+                return;
+            }
+            *x -= f / f_prime;
+            f_prime = *x * *x * *x * four + *x * p * two + q;
+            f = *x * *x * *x * *x + *x * *x * p + *x * q + r;
+        }
+    });
+    res
 }
 
 /// Solve x^4 + ax^3 + bx^2 + cx + d = 0.
-pub fn solve_quartic<F: BaseFloat>(_a: F, _b: F, _c: F, _d: F) -> [Complex<F>; 4] {
-    todo!()
+pub fn solve_quartic<F: BaseFloat>(a: F, b: F, c: F, d: F) -> [Complex<F>; 4] {
+    let one = F::one();
+    let two = one + one;
+    let three = one + two;
+    let four = two + two;
+    let six = two * three;
+    let eight = four + four;
+
+    let a_4 = a / four;
+    let p = b - six * a_4 * a_4;
+    let q = c - two * b * a_4 + eight * a_4 * a_4 * a_4;
+    let r = d - c * a_4 + b * a_4 * a_4 - three * a_4 * a_4 * a_4 * a_4;
+    let mut res = pre_solve_quartic(p, q, r);
+    res.iter_mut().for_each(|x| *x -= a_4);
+    res
 }
 
 #[cfg(test)]
